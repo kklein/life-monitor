890b5fbe7ccf29d66de4d43a8c8b333488e3bc2d
kklein <kleinkevin.lu@gmail.com>
2022-06-24 12:33:06 +0200

    Provide some unit tests for cal methods.

---
+++ tests/test_cal.py
@@ -0,0 +1,151 @@
+import os
+
+# TODO: Get rid of path hack.
+import sys
+
+import pytest
+
+from monitoring import cal, utils
+
+sys.path.insert(0, os.path.abspath(".."))
+
+
+@pytest.fixture(scope="module")
+def events():
+    return [
+        {
+            "kind": "calendar#event",
+            "created": "2020-01-01T21:34:53.000Z",
+            "updated": "2020-01-01T21:34:53.480Z",
+            "summary": "Running",
+            "description": "14.91 km",
+            "colorId": "4",
+            "start": {
+                "dateTime": "2020-01-01T08:30:00+01:00",
+                "timeZone": "Europe/Rome",
+            },
+            "end": {"dateTime": "2020-01-01T10:00:00+01:00", "timeZone": "Europe/Rome"},
+            "eventType": "default",
+        },
+        {
+            "kind": "calendar#event",
+            "created": "2020-01-02T21:34:53.000Z",
+            "updated": "2020-01-02T21:34:53.480Z",
+            "summary": "Gym",
+            "colorId": "4",
+            "start": {
+                "dateTime": "2020-01-02T08:30:00+01:00",
+                "timeZone": "Europe/Rome",
+            },
+            "end": {"dateTime": "2020-01-02T10:00:00+01:00", "timeZone": "Europe/Rome"},
+            "eventType": "default",
+        },
+        {
+            "kind": "calendar#event",
+            "created": "2020-01-03T21:34:53.000Z",
+            "updated": "2020-01-03T21:34:53.480Z",
+            "summary": "Swimming",
+            "description": ".6km",
+            "colorId": "4",
+            "start": {
+                "dateTime": "2020-01-03T08:30:00+01:00",
+                "timeZone": "Europe/Rome",
+            },
+            "end": {"dateTime": "2020-01-03T10:00:00+01:00", "timeZone": "Europe/Rome"},
+            "eventType": "default",
+        },
+        {
+            "kind": "calendar#event",
+            "created": "2020-01-04T21:34:53.000Z",
+            "updated": "2020-01-04T21:34:53.480Z",
+            "summary": "Gym: ub",
+            "colorId": "4",
+            "start": {
+                "dateTime": "2020-01-04T08:30:00+01:00",
+                "timeZone": "Europe/Rome",
+            },
+            "end": {"dateTime": "2020-01-04T10:00:00+01:00", "timeZone": "Europe/Rome"},
+            "eventType": "default",
+        },
+    ]
+
+
+def test_smoke_get_getdataframe(events):
+    cal.get_dataframe(events)
+
+
+@pytest.mark.parametrize(
+    "data",
+    [
+        (utils.Sport.running, 1),
+        (utils.Sport.gym, 2),
+        (utils.Sport.gym_ub, 1),
+        (utils.Sport.cycling, 0),
+    ],
+)
+def test_get_summary_filter(data, events):
+    sport, expected_count = data
+    filter_function = cal._get_summary_filter(sport)
+    actual_count = len(list(filter(filter_function, events)))
+    assert actual_count == expected_count
+
+
+@pytest.mark.parametrize("sport", [utils.Sport.swimming, utils.Sport.running])
+def test_prune_events_distance(events, sport):
+    filter_function = cal._get_summary_filter(sport)
+    sport_events = list(filter(filter_function, events))
+    pruned_events = cal.prune_events(sport_events)
+    for event in pruned_events:
+        assert isinstance(event["distance"], float)
+
+
+@pytest.mark.parametrize(
+    "flawed_event",
+    [
+        # Invalid sport kind.
+        {
+            "kind": "calendar#event",
+            "created": "2020-01-01T21:34:53.000Z",
+            "updated": "2020-01-01T21:34:53.480Z",
+            "summary": "Running w/ Bob",
+            "description": "14.91 km",
+            "colorId": "4",
+            "start": {
+                "dateTime": "2020-01-01T08:30:00+01:00",
+                "timeZone": "Europe/Rome",
+            },
+            "end": {"dateTime": "2020-01-01T10:00:00+01:00", "timeZone": "Europe/Rome"},
+            "eventType": "default",
+        },
+        # Distance not convertible.
+        {
+            "kind": "calendar#event",
+            "created": "2020-01-01T21:34:53.000Z",
+            "updated": "2020-01-01T21:34:53.480Z",
+            "summary": "Running",
+            "description": "14.a91 km",
+            "colorId": "4",
+            "start": {
+                "dateTime": "2020-01-01T08:30:00+01:00",
+                "timeZone": "Europe/Rome",
+            },
+            "end": {"dateTime": "2020-01-01T10:00:00+01:00", "timeZone": "Europe/Rome"},
+            "eventType": "default",
+        },
+        # dateTime filed not a valid Datetime
+        {
+            "kind": "calendar#event",
+            "created": "2020-01-01T21:34:53.000Z",
+            "updated": "2020-01-01T21:34:53.480Z",
+            "summary": "Running",
+            "description": "14.91 km",
+            "colorId": "4",
+            "start": {"dateTime": "2020-01-z8:30:00+01:00", "timeZone": "Europe/Rome"},
+            "end": {"dateTime": "2020-01-01T10:00:00+01:00", "timeZone": "Europe/Rome"},
+            "eventType": "default",
+        },
+    ],
+)
+def test_prune_events_failure(flawed_event):
+    with pytest.raises(Exception):
+        cal.prune_events([flawed_event])
