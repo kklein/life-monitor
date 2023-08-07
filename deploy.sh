6a062977c14cc5d363fbe073822d90cd23c1a0e0
kklein <kleinkevin.lu@gmail.com>
2023-08-07 07:58:44 +0200

    Fix deployment script.

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,19 @@
-gcloud functions deploy f_scheduled --entry-point main --runtime python39 --trigger-resource ping_schedule --trigger-event google.pubsub.topic.publish --timeout 540s --region europe-west3 --env-vars-file env-vars.yaml
+#!/bin/bash
 
-gcloud scheduler jobs delete pubsub_cal_weekly --location europe-west3
-gcloud scheduler jobs delete pubsub_cal_daily --location europe-west3
-gcloud scheduler jobs delete pubsub_org_weekly --location europe-west3
-gcloud scheduler jobs delete pubsub_org_daily --location europe-west3
+set -e
 
-gcloud scheduler jobs create pubsub pubsub_cal_weekly --schedule "10 16 * * SUN" --topic ping_schedule --message-body '{"kind": "calendar_weekly"}' --location europe-west3
+gcloud functions deploy f_scheduled --entry-point main --runtime python39 --trigger-resource ping_schedule --trigger-event google.pubsub.topic.publish --timeout 540s --region europe-west3 --env-vars-file env-vars.yaml --project stupid-schedule
 
-gcloud scheduler jobs create pubsub pubsub_cal_daily --schedule "0 19 * * *" --topic ping_schedule --message-body '{"kind": "calendar_daily"}' --location europe-west3
+gcloud scheduler jobs delete pubsub_cal_weekly --location europe-west3 --project stupid-schedule
+gcloud scheduler jobs delete pubsub_cal_daily --location europe-west3 --project stupid-schedule
+gcloud scheduler jobs delete pubsub_org_weekly --location europe-west3 --project stupid-schedule
+gcloud scheduler jobs delete pubsub_org_daily --location europe-west3 --project stupid-schedule
 
-gcloud scheduler jobs create pubsub pubsub_org_weekly --schedule "0 16 * * SUN" --topic ping_schedule --message-body '{"kind": "org_weekly"}' --location europe-west3
+gcloud scheduler jobs create pubsub pubsub_cal_weekly --schedule "10 16 * * SUN" --topic projects/stupid-schedule/topics/ping_schedule --message-body '{"kind": "calendar_weekly"}' --location europe-west3 --project stupid-schedule
 
-gcloud scheduler jobs create pubsub pubsub_org_daily --schedule "0 7 * * *" --topic ping_schedule --message-body '{"kind": "org_daily"}' --location europe-west3
+gcloud scheduler jobs create pubsub pubsub_cal_daily --schedule "0 19 * * *" --topic projects/stupid-schedule/topics/ping_schedule --message-body '{"kind": "calendar_daily"}' --location europe-west3 --project stupid-schedule
+
+gcloud scheduler jobs create pubsub pubsub_org_weekly --schedule "0 16 * * SUN" --topic projects/stupid-schedule/topics/ping_schedule --message-body '{"kind": "org_weekly"}' --location europe-west3 --project stupid-schedule
+
+gcloud scheduler jobs create pubsub pubsub_org_daily --schedule "0 7 * * *" --topic projects/stupid-schedule/topics/ping_schedule --message-body '{"kind": "org_daily"}' --location europe-west3 --project stupid-schedule
 
