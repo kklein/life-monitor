005226e138ee6be7580c53602f9f5c4dcc742728
kklein <kleinkevin.lu@gmail.com>
2023-08-07 06:26:31 +0200

    Accept non-distance descriptions.

--- monitoring/cal.py
+++ monitoring/cal.py
@@ -143,7 +143,7 @@ def prune_events(events):
         {
             "date_string": event["start"]["dateTime"],
             "distance": event["description"].split("km")[0]
-            if "description" in event
+            if "description" in event and "km" in event["description"]
             else None,
             "title": event["summary"].lower(),
         }
